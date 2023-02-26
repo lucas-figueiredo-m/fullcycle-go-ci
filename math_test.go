a8291a53fde330e788e0a656f171f0e18a202713
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2023-02-25 22:42:11 -0300

    feat: changes message on SomaTest

--- math_test.go
+++ math_test.go
@@ -6,6 +6,6 @@ func TestSoma(t *testing.T) {
 	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da soma é inválido:\nResultado %d.\nEsperado: %d", total, 30)
+		t.Errorf("\nResultado da soma é inválido:\nResultado %d.\nEsperado: %d", total, 30)
 	}
 }
