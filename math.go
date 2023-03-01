3ae5b4d7870c126d97322674f79e305a2ee171ca
Lucas Figueiredo <lucas.figueiredo@engenharia.ufjf.br>
2023-02-28 22:46:49 -0300

    ci: check if quality gate work on sonar cloud

--- math.go
+++ math.go
@@ -10,26 +10,26 @@ func soma(a int, b int) int {
 	return a + b
 }
 
-func sub(a int, b int) int {
-	return a - b
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
 
-func times(a int, b int) int {
-	return a * b
-}
+// func times(a int, b int) int {
+// 	return a * b
+// }
 
-func somaX(a int, b int) int {
-	return a*2 + b
-}
+// func somaX(a int, b int) int {
+// 	return a*2 + b
+// }
 
-func subX(a int, b int) int {
-	return b - a*2
-}
+// func subX(a int, b int) int {
+// 	return b - a*2
+// }
 
-func square(a int, b int) int {
-	return a * a
-}
+// func square(a int, b int) int {
+// 	return a * a
+// }
 
-func cubic(a int, b int) int {
-	return a * a * a
-}
+// func cubic(a int, b int) int {
+// 	return a * a * a
+// }
